20c493bdd6213c368de11fa895d1552d259b1b0c
Ignatius22 <igee54077@gmail.com>
2020-09-15 08:38:04 +0100

    assign player turn method

--- JS/app.js
+++ JS/app.js
@@ -1,3 +1,13 @@
 const playerFactory = (name, mark) => {
+	  const playTurn = (board, cell) => {
+    const idx = board.cells.findIndex(position => position === cell);
+    if (board.boardArray[idx] === '') {
+      board.render();
+      return idx;
+    }
+    return null;
+  };
+
+  return { name, mark, playTurn };
 
 };
