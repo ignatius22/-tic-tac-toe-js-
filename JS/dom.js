f355e5b73cb28f2815a12fce796082276bd8fc65
Ignatius22 <igee54077@gmail.com>
2020-09-16 02:23:02 +0100

    separate logic from dom

--- JS/dom.js
+++ JS/dom.js
@@ -1,62 +1,3 @@
-const playerFactory = (name, mark) => {
-  const playTurn = (board, cell) => {
-    const idx = board.cells.findIndex(position => position === cell);
-    if (board.boardArray[idx] === '') {
-      board.render();
-      return idx;
-    }
-    return null;
-  };
-
-  return { name, mark, playTurn };
-};
-
-
-const boardModule = (() => {
-  let boardArray = ['', '', '', '', '', '', '', '', ''];
-  const gameBoard = document.querySelector('#board');
-  const cells = Array.from(document.querySelectorAll('.cell'));
-  let winner = null;
-
-  const render = () => {
-    boardArray.forEach((mark, idx) => {
-      cells[idx].textContent = boardArray[idx];
-    });
-  };
-  const reset = () => {
-    boardArray = ['', '', '', '', '', '', '', '', ''];
-  };
-  const checkWin = () => {
-    const winArrays = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-
-    winArrays.forEach((combo) => {
-      if (boardArray[combo[0]]
-                && boardArray[combo[0]] === boardArray[combo[1]]
-                && boardArray[combo[0]] === boardArray[combo[2]]) {
-        winner = 'current';
-      }
-    });
-    return winner || (boardArray.includes('') ? null : 'Tie');
-  };
-  return {
-    render,
-    gameBoard,
-    cells,
-    boardArray,
-    checkWin,
-    reset,
-  };
-})();
-
 const gamePlay = (() => {
   const playerOneName = document.querySelector('#player1');
   const playerTwoName = document.querySelector('#player2');
@@ -97,6 +38,7 @@ const gamePlay = (() => {
       }
     });
   };
+
   const gameInit = () => {
     if (playerOneName.value !== '' && playerTwoName.value !== '') {
       playerOne = playerFactory(playerOneName.value, 'X');
@@ -105,7 +47,6 @@ const gamePlay = (() => {
       gameRound();
     }
   };
-
   form.addEventListener('submit', (event) => {
     event.preventDefault();
     if (playerOneName.value !== '' && playerTwoName.value !== '') {
@@ -125,6 +66,6 @@ const gamePlay = (() => {
     return {
       gameInit,
     };
+    gamePlay.gameInit();
   });
-  gamePlay.gameInit();
 })();
